d18cfc5447d42789716a49e4129dee00aa516fca
Bahadir Iren <64319816+BahadirIren@users.noreply.github.com>
2021-08-20 11:44:07 +0300

    Delete 1-Basic-Syntax directory

--- 1-Basic-Syntax/3-Arithmetic_Operators_BODMAS.js
+++
@@ -1,5 +0,0 @@
-// assigment operator
-var name = "Bahadir";
-
-// precedence B O DM AS => brackets (), p0wers, division and multiplication, addition and subtraction,  
-console.log(Math.pow(2, 4) / 2 + 1 - 5 + (2 * 12));
